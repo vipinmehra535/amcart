c317084d0ae3c0d0440ce291612c13a8be3088cc
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-03-16 22:17:16 +0530

    Add search services to fetch products based on search query

---
+++ lib/features/search/services/search_services.dart
@@ -0,0 +1,49 @@
+import 'package:amcart/models/product.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:amcart/constants/error_handling.dart';
+import 'package:amcart/constants/global_variables.dart';
+import 'package:amcart/constants/utlis.dart';
+import 'package:amcart/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+class SearchServices {
+  Future<List<Product>> fetchSearchProduct({
+    required BuildContext context,
+    required String searchQuery,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Product> productList = [];
+    try {
+      http.Response res = await http
+          .get(Uri.parse('$uri/api/products?search=$searchQuery'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      if (context.mounted) {
+        httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            for (int i = 0; i < jsonDecode(res.body).length; i++) {
+              productList.add(
+                Product.fromJson(
+                  jsonEncode(
+                    jsonDecode(res.body)[i],
+                  ),
+                ),
+              );
+            }
+          },
+        );
+      }
+    } catch (e) {
+      if (context.mounted) {
+        showSnackBar(context, e.toString());
+      }
+    }
+    return productList;
+  }
+}
