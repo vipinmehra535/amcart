94608051e2792bcef4c30dee77bf8927c089c8c3
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-03-17 10:07:40 +0530

    Add endpoint to fetch products by category

--- server/routes/product.js
+++ server/routes/product.js
@@ -13,4 +13,14 @@ productRouter.get("/api/products", auth, async (req, res) => {
   }
 });
 
+productRouter.get("/api/products/:category", auth, async (req, res) => {
+  try {
+    console.log(req.query.category);
+    const products = await Product.find({ category: req.query.category });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = productRouter;
