f49819d464008916e75bc536185a87a8739b8d8b
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-04-11 13:27:31 +0530

    Implement add-to-cart endpoint in user router

--- server/routes/user..js
+++ server/routes/user..js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
 
-userRouter.post();
+userRouter.post("/api/add-to-cart", async (req, res) => {});
 
 module.exports = userRouter;
