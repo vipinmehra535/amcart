f8dbd5ba8e3143917a33577c65a3cc1d241d0811
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-04-11 20:38:58 +0530

    Remove debug logging for deal of the day product retrieval

--- server/routes/product.js
+++ server/routes/product.js
@@ -70,7 +70,6 @@ productRouter.get("/api/deal-of-day", auth, async (req, res) => {
       return aSum < bSum ? 1 : -1;
     });
 
-    console.log(products[0]);
     res.json(products[0]);
   } catch (e) {
     res.status(500).json({ error: e.message });
