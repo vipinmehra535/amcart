79dfbd051e9ea4b42d282c88cbb56b5c4e4a3081
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-04-11 15:43:16 +0530

    Add CartScreen and CartSubtotal components; update BottomBar to include CartScreen

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amcart/constants/global_variables.dart';
 import 'package:amcart/features/account/screens/account_screen.dart';
+import 'package:amcart/features/cart/screens/cart_screen.dart';
 import 'package:amcart/features/home/screens/home_screen.dart';
 import 'package:amcart/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,7 @@ class _BottomBarState extends State<BottomBar> {
   List<Widget> pages = [
     const HomeScreen(),
     const AccountScreen(),
-    const Center(child: Text("Cart Screen")),
+    const CartScreen(),
   ];
   void updatepage(value) {
     setState(() {
