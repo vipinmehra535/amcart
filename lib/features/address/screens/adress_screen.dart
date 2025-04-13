25ae98d29f8441ea7b78088d9df40a3891cc03f6
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-04-13 22:10:36 +0530

    Add payment configuration for Apple Pay and Google Pay; implement payment item handling in AddressScreen

--- lib/features/address/screens/adress_screen.dart
+++ lib/features/address/screens/adress_screen.dart
@@ -115,6 +115,21 @@ class _AddressScreenState extends State<AddressScreen> {
   @override
   Widget build(BuildContext context) {
     var address = context.watch<UserProvider>().user.address;
+    const _paymentItems = [
+      PaymentItem(
+        label: 'Total',
+        amount: '99.99',
+        status: PaymentItemStatus.final_price,
+      )
+    ];
+
+    void onApplePayResult(paymentResult) {
+      // Send the resulting Apple Pay token to your server / PSP
+    }
+
+    void onGooglePayResult(paymentResult) {
+      // Send the resulting Google Pay token to your server / PSP
+    }
 
     return Scaffold(
       appBar: PreferredSize(
@@ -190,17 +205,38 @@ class _AddressScreenState extends State<AddressScreen> {
                   ],
                 ),
               ),
-              ApplePayButton(
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 10,
                 width: double.infinity,
-                style: ApplePayButtonStyle.whiteOutline,
-                type: ApplePayButtonType.buy,
-                paymentConfiguration: _paymentConfiguration!,
-                onPaymentResult: onApplePayResult,
-                paymentItems: paymentItems,
-                margin: const EdgeInsets.only(top: 15),
-                height: 50,
-                // onPressed: () => payPressed(address),
               ),
+              // ApplePayButton(
+              //   paymentConfiguration: defaultApplePayConfig,
+              //   paymentItems: _paymentItems,
+              //   style: ApplePayButtonStyle.black,
+              //   type: ApplePayButtonType.buy,
+              //   margin: const EdgeInsets.only(top: 15.0),
+              //   onPaymentResult: onApplePayResult,
+              //   loadingIndicator: const Center(
+              //     child: CircularProgressIndicator(),
+              //   ),
+              // ),
+              GooglePayButton(
+                width: double.infinity,
+                paymentConfiguration: defaultGooglePayConfig,
+                paymentItems: _paymentItems,
+                type: GooglePayButtonType.buy,
+                margin: const EdgeInsets.only(top: 15.0),
+                onPaymentResult: onGooglePayResult,
+                loadingIndicator: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+
               const SizedBox(height: 10),
               // GooglePayButton(
               //   onPressed: () => payPressed(address),
@@ -222,3 +258,181 @@ class _AddressScreenState extends State<AddressScreen> {
     );
   }
 }
+
+/// Sample [PaymentConfiguration] for Apple Pay
+final defaultApplePayConfig =
+    PaymentConfiguration.fromJsonString(defaultApplePay);
+
+/// Sample configuration for Apple Pay. Contains the same content as the file
+/// under `assets/default_payment_profile_apple_pay.json`.
+const String defaultApplePay = '''{
+  "provider": "apple_pay",
+  "data": {
+    "merchantIdentifier": "merchant.com.sams.fish",
+    "displayName": "Sam's Fish",
+    "merchantCapabilities": ["3DS", "debit", "credit"],
+    "supportedNetworks": ["amex", "visa", "discover", "masterCard"],
+    "countryCode": "US",
+    "currencyCode": "USD",
+    "requiredBillingContactFields": ["emailAddress", "name", "phoneNumber", "postalAddress"],
+    "requiredShippingContactFields": [],
+    "shippingMethods": [
+      {
+        "amount": "0.00",
+        "detail": "Available within an hour",
+        "identifier": "in_store_pickup",
+        "label": "In-Store Pickup"
+      },
+      {
+        "amount": "4.99",
+        "detail": "5-8 Business Days",
+        "identifier": "flat_rate_shipping_id_2",
+        "label": "UPS Ground"
+      },
+      {
+        "amount": "29.99",
+        "detail": "1-3 Business Days",
+        "identifier": "flat_rate_shipping_id_1",
+        "label": "FedEx Priority Mail"
+      }
+    ]
+  }
+}''';
+
+/// Sample [PaymentConfiguration] for Google Pay
+final defaultGooglePayConfig =
+    PaymentConfiguration.fromJsonString(defaultGooglePay);
+
+/// Sample configuration for Google Pay. Contains the same content as the file
+/// under `assets/default_payment_profile_google_pay.json`.
+const String defaultGooglePay = '''{
+  "provider": "google_pay",
+  "data": {
+    "environment": "TEST",
+    "apiVersion": 2,
+    "apiVersionMinor": 0,
+    "allowedPaymentMethods": [
+      {
+        "type": "CARD",
+        "tokenizationSpecification": {
+          "type": "PAYMENT_GATEWAY",
+          "parameters": {
+            "gateway": "example",
+            "gatewayMerchantId": "exampleGatewayMerchantId"
+          }
+        },
+        "parameters": {
+          "allowedCardNetworks": ["VISA", "MASTERCARD"],
+          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          "billingAddressRequired": true,
+          "billingAddressParameters": {
+            "format": "FULL",
+            "phoneNumberRequired": true
+          }
+        }
+      }
+    ],
+    "merchantInfo": {
+      "merchantName": "Example Merchant Name"
+    },
+    "transactionInfo": {
+      "countryCode": "US",
+      "currencyCode": "USD"
+    }
+  }
+}''';
+
+const String basicGooglePayIsReadyToPay = '''{
+  "provider": "google_pay",
+  "data": {
+    "apiVersion": 2,
+    "apiVersionMinor": 0,
+    "allowedPaymentMethods": [
+      {
+        "type": "CARD",
+        "parameters": {
+          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          "allowedCardNetworks": ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+        }
+      }
+    ]
+  }
+}''';
+
+const String basicGooglePayLoadPaymentData = '''{
+  "provider": "google_pay",
+  "data": {
+    "apiVersion": 2,
+    "apiVersionMinor": 0,
+    "merchantInfo": {
+      "merchantName": "Example Merchant"
+    },
+    "allowedPaymentMethods": [
+      {
+        "type": "CARD",
+        "parameters": {
+          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          "allowedCardNetworks": ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+        },
+        "tokenizationSpecification": {
+          "type": "PAYMENT_GATEWAY",
+          "parameters": {
+            "gateway": "example",
+            "gatewayMerchantId": "exampleGatewayMerchantId"
+          }
+        }
+      }
+    ],
+    "transactionInfo": {
+      "totalPriceStatus": "FINAL",
+      "totalPrice": "12.34",
+      "currencyCode": "USD"
+    }
+  }
+}''';
+
+const String invalidGooglePayIsReadyToPay = '''{
+  "provider": "google_pay",
+  "data": {
+    "apiVersion": 2,
+    "apiVersionMinor": 0,
+    "allowedPaymentMethods": [
+      {
+        "type": "CARD",
+        "parameters": {}
+      }
+    ]
+  }
+}''';
+
+const String invalidGooglePayLoadPaymentData = '''{
+  "provider": "google_pay",
+  "data": {
+    "apiVersion": 2,
+    "apiVersionMinor": 0,
+    "merchantInfo": {
+      "merchantName": "Example Merchant"
+    },
+    "allowedPaymentMethods": [
+      {
+        "type": "CARD",
+        "parameters": {
+          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          "allowedCardNetworks": ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+        },
+        "tokenizationSpecification": {
+          "type": "PAYMENT_GATEWAY",
+          "parameters": {
+            "gateway": "example",
+            "gatewayMerchantId": "exampleGatewayMerchantId"
+          }
+        }
+      }
+    ],
+    "transactionInfo": {
+      "totalPriceStatus": "FINAL",
+      "totalPrice": "12.34",
+      "currencyCode": "USD"
+    }
+  }
+}''';
